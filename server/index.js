ad85cf2f36843f52e0df58f9edfe357b06f8fcbf
GASTON-YAPURA <gastonyapura61@gmail.com>
2026-06-29 16:36:34 -0300

    fix: agregar URL de Vercel al CORS del backend

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'https://tpf-dw-edsel-irupe.vercel.app',
+    'https://tpf-dw-edsel-irupe-git-main-gaston-yapura-s-projects.vercel.app',
   ],
   methods: ['GET', 'POST'],
 }))
